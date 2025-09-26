9c0565fdf74c3f6a54c03e79180b23f1bf57a26a
hendisantika <hendisantika@yahoo.co.id>
2025-09-27 06:52:45 +0700

    docs: Add createUser in UserController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/elasticapmlogstash/controller/UserController.java
+++ src/main/java/id/my/hendisantika/elasticapmlogstash/controller/UserController.java
@@ -2,11 +2,15 @@ package id.my.hendisantika.elasticapmlogstash.controller;
 
 import id.my.hendisantika.elasticapmlogstash.entity.User;
 import id.my.hendisantika.elasticapmlogstash.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +54,20 @@ public class UserController {
             return ResponseEntity.ok(user.get());
         } else {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    @CaptureTransaction("create-user")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        log.info("POST /api/users - Creating new user");
+
+        try {
+            User createdUser = userService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (RuntimeException e) {
+            log.error("Error creating user: {}", e.getMessage());
+            return ResponseEntity.badRequest().build();
         }
     }
 }
