abbf32cbb4bec7d1db771e3b0877ca62c0f39d6d
hendisantika <hendisantika@yahoo.co.id>
2025-09-27 06:25:50 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/elasticapmlogstash/SpringBootElasticApmLogstashApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elasticapmlogstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticApmLogstashApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootElasticApmLogstashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/elasticapmlogstash/SpringBootElasticApmLogstashApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elasticapmlogstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticApmLogstashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
