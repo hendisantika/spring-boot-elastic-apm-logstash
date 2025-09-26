b569329dd0ee4ed24748b1c3a77af532755fa4e1
hendisantika <hendisantika@yahoo.co.id>
2025-09-27 06:27:13 +0700

    feat: @EnableAsync & @EnableTransactionManagement🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/elasticapmlogstash/SpringBootElasticApmLogstashApplication.java
+++ src/main/java/id/my/hendisantika/elasticapmlogstash/SpringBootElasticApmLogstashApplication.java
@@ -2,8 +2,12 @@ package id.my.hendisantika.elasticapmlogstash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAsync
+@EnableTransactionManagement
 public class SpringBootElasticApmLogstashApplication {
 
     static void main(String[] args) {
